6c0bd66ba9368fe17ee462233d55abf8a9818532
Paige Watson <paige.watson@outlook.com>
2022-06-29 18:57:07 -0700

    add test for getAllStories

--- src/state-management/thunks/__tests__/getAllStories.test.ts
+++ src/state-management/thunks/__tests__/getAllStories.test.ts
@@ -1,5 +1,14 @@
+import { FakeDataService } from '../../../configuration/FakeDataService';
+import { createGetAllStories, getAllStories } from '../getAllStories';
+import { mockInitialStore } from '../../../configuration/Store';
+import { testStoryInformationArray } from '../../../configuration/fixtures/StoryFixtureData';
+
 describe('getAllStories', () => {
-    it('should call StoryService to get data', () => {
+    it('should return array of StoryInformation', async () => {
+        const dataService = new FakeDataService();
+        const getAlLStories = createGetAllStories(dataService);
+        const returnedAction = await mockInitialStore.dispatch(getAlLStories());
+        expect(returnedAction.payload).toEqual(testStoryInformationArray);
 
     });
 });
