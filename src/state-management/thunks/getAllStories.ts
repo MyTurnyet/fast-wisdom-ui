954445c36249af6e31cfd8f150341108631ed379
Paige Watson <paige.watson@outlook.com>
2022-06-29 18:20:33 -0700

    add getAllStories thunk

--- src/state-management/thunks/getAllStories.ts
+++ src/state-management/thunks/getAllStories.ts
@@ -1,6 +1,18 @@
-import { StoryDataService } from '../../services/StoryDataService';
+import { StoryDataService, StoryInformation } from '../../services/StoryDataService';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+
+export interface ThunkStoreApi {
+    state: RootState;
+}
 
 export const createGetAllStories = (service: StoryDataService) => {
-    return createAsyncThunk()
+    return createAsyncThunk<StoryInformation[], void, ThunkStoreApi>(
+        'data/allStories',
+        async () => {
+            return await service.getAllStories();
+        }
+    )
 }
+
+export const getAllStories = createGetAllStories(new StoryDataService());
