f33863295c9d996d63d87dbaf2f6cf3f42913d20
Paige Watson <paige.watson@outlook.com>
2022-07-10 15:52:17 -0700

    add Main Page

---
+++ src/pages/__tests__/MainPage.test.tsx
@@ -0,0 +1,10 @@
+import { MainPage } from '../MainPage';
+import { render } from '@testing-library/react';
+import { hasExpectedElementsByText } from '../../configuration/ReactTestToolkit';
+
+describe('MainPage', () => {
+    it('renders just the name', () => {
+        const view = render(<MainPage></MainPage>);
+        hasExpectedElementsByText(view, 'Main Page');
+    });
+});
