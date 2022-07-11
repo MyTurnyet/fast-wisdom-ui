c9870ac2d967c171f8d33a9bc11c5d0096bdb91d
Paige Watson <paige@industriallogic.com>
2022-07-11 11:05:20 -0700

    remove bad test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router';
+import {Navigate, Route, Routes} from 'react-router';
 import './App.scss';
-import { AppLayout } from './pages/AppLayout';
-import { MainPage } from './pages/MainPage';
-import { StoryDisplay } from './pages/subpages/StoryDisplay';
+import {AppLayout} from './pages/AppLayout';
+import {MainPage} from './pages/MainPage';
+import {StoryDisplay} from './pages/subpages/StoryDisplay';
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<AppLayout/>}>
                 <Route index={true} element={<Navigate to={'/home'}/>}/>
-                <Route path="home" element={<MainPage/>}/>
-                <Route path="story" element={<StoryDisplay/>}>
+                <Route path="/home" element={<MainPage/>}/>
+                <Route path="/story" element={<StoryDisplay/>}>
                     <Route path=":storyId" element={<StoryDisplay/>}/>
                 </Route>
                 {/*<Route path="car-types" element={<CarTypesPage />} />*/}
--- src/pages/__tests__/AppLayout.test.tsx
+++ src/pages/__tests__/AppLayout.test.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { AppLayout } from '../AppLayout';
-import { hasExpectedElementsByText, renderWithRouterAndStore } from '../../configuration/ReactTestToolkit';
-import { mockInitialStore, mockStoreWithOneStory } from '../../configuration/Store';
+import {AppLayout} from '../AppLayout';
+import {hasExpectedElementsByText, renderWithRouterAndStore} from '../../configuration/ReactTestToolkit';
+import {mockStoreWithOneStory} from '../../configuration/Store';
 
 describe('App Layout', () => {
-    xit('has Main Page', () => {
-        const view = renderWithRouterAndStore(<AppLayout/>, mockInitialStore);
-        hasExpectedElementsByText(view, 'Main Page');
-    });
     it('has Story 1 title in left nav', () => {
         const view = renderWithRouterAndStore(<AppLayout/>, mockStoreWithOneStory);
         hasExpectedElementsByText(view, 'Title of Story 1');
