7fa056a6b519143eb958abed25d7c5536000e345
Paige Watson <paige.watson@outlook.com>
2022-07-11 22:28:04 -0700

    add css classes to display

--- src/pages/subpages/StoryDisplay.tsx
+++ src/pages/subpages/StoryDisplay.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
-import {useAppSelector} from '../../state-management/hooks';
-import {StoryInformation} from '../../services/StoryDataService';
+import { useParams } from 'react-router-dom';
+import { useAppSelector } from '../../state-management/hooks';
+import { StoryInformation } from '../../services/StoryDataService';
 import './StoryDisplay.scss';
+
 export const StoryDisplay = () => {
     let params = useParams();
     const emptyStory: StoryInformation = {
@@ -16,10 +17,22 @@ export const StoryDisplay = () => {
     const storyToDisplay = allStories.find(value => value.storyId === params.storyId) ?? emptyStory;
     return (
         <div className={'StoryDisplay'}>
-            <div className={'title'} title={'title'}>{storyToDisplay.title}</div>
-            <div title={'description'}>{storyToDisplay.description}</div>
-            <div title={'voteCount'}>{storyToDisplay.numberOfVotes}</div>
-            <div title={'voteAverage'}>{storyToDisplay.voteAverage}</div>
+            <div className={'text-data'}>
+                <span className={'label'}>Title:</span>
+                <span title={'title'}>{storyToDisplay.title}</span>
+            </div>
+            <div className={'text-data'}>
+                <div className={'label'}>Description:</div>
+                <span title={'description'}>{storyToDisplay.description}</span>
+            </div>
+            <div className={'text-data'}>
+                <span className={'label'}>Number of Votes:</span>
+                <span title={'voteCount'}>{storyToDisplay.numberOfVotes}</span>
+            </div>
+            <div className={'text-data'}>
+                <span className={'label'}>Average:</span>
+                <span title={'voteAverage'}>{storyToDisplay.voteAverage}</span>
+            </div>
         </div>
     );
 }
